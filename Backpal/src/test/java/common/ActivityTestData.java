a7b87684a04695b7360a27d8046e1505fb111fb1
gabrielaramburu <gabrielaramburu@gmail.com>
2023-01-20 11:03:03 -0300

    adding test case for domain

--- Backpal/src/test/java/common/ActivityTestData.java
+++ Backpal/src/test/java/common/ActivityTestData.java
@@ -1,5 +1,28 @@
 package common;
 
-public class ActivityTestData {
+import java.time.LocalDateTime;
 
+import account.model.Account;
+import account.model.Money;
+import account.model.Activity.ActivityId;
+import lombok.NonNull;
+
+public class ActivityTestData {
+	public static class ActivityBuilder {
+		private ActivityId id;
+		private Account.AccountId ownerAccount;
+		private Account.AccountId sourceAccount;
+		private Account.AccountId targetAccount;
+		private LocalDateTime timestamp;
+		
+		public ActivityBuilder withActivityId(ActivityId id) {
+			this.id = id;
+			return this;
+		}
+		
+		public ActivityBuilder withOwnerAccount(Account.AccountId ownerId) {
+			this.ownerAccount =ownerId;
+			return this;
+		}
+	}
 }
